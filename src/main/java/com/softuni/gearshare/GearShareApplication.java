f7be4af6d4549da01096735f01573ed8f9b2ef19
BozhidarMladenov <bozhidar.mladenov@gmail.com>
2026-06-20 14:30:13 +0300

    chore: initialize Spring Boot project with dependencies

---
+++ src/main/java/com/softuni/gearshare/GearShareApplication.java
@@ -0,0 +1,12 @@
+package com.softuni.gearshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GearShareApplication.class, args);
+    }
+}
